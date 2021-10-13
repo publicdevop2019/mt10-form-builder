9a7408f55d5c613982f09f83f64ceb8d8a6a3b6c
publicdevop2019 <publicdevop2019hw@gmail.com>
2021-10-13 17:39:12 -0400

    export factory component

--- projects/mt-form-builder/src/public_api.ts
+++ projects/mt-form-builder/src/public_api.ts
@@ -5,3 +5,4 @@
 export * from './lib/mt-form-builder.module';
 export * from './lib/components/lib/text-input/text-input.component';
 export * from './lib/services/form-info.service';
+export * from './lib/components/factory/factory.component';
